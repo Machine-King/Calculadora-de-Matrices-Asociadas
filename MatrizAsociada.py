9522f612c89537ddc3dd22cda0f5e31e24e6e1e4
Machine-King <al416715@uji.es>
2022-01-30 00:39:24 +0100

    Primera version funcional V1.0

---
+++ MatrizAsociada.py
@@ -0,0 +1,72 @@
+# VERSION 1.0 (28-Enero-2022)
+#
+# PACKAGES usados:
+#   -sympy 1.9
+# RESOURCES usados:
+#   -ModuloMatrices_Aolicaciones.py
+# AYUDA del programa MatrizAsociada.p:
+#
+# El formato a utilizar viene dado al principio. Es IMPORTANTE respetar los espacios para evitar errores
+
+import sys
+from ModuloMatrices_Aplicaciones import *
+from sympy import *
+
+print("**Bienvenido a Calculadora Avanzada de Matrices Asociadas**")
+print("\nFormato introduccion de datos: ")
+print("\t-Aplicación lineal: Cada elemento del espacio de partida y de llegada se separa con una coma (sin espacios)")
+print("\t Ejemplo: (x,y,z) -> (x+2*y+z,y,z)\n")
+print("\t-Bases: Cada vector de una base se separa con espacios y los elementos de cada vector mediante comas)")
+print("\t Ejemplo: {(1,2,3) (1,0,0) (0,0,1)}\n")
+
+aplicacion = input("Introduce la aplicación f : ")
+base1 = input("Introduce una base del (sub)espacio vectorial de partida V : ")
+base2 = input("Introduce una base del (sub)espacio vectorial de llegada W : ")
+datosApl = leerAplicacionLineal(aplicacion)
+Esp1 = datosApl[0]
+Esp2 = datosApl[1]
+base1 = leerBase(base1)
+base2 = leerBase(base2)
+matrizAsociada = []
+dim_base1 = len(base1)
+dim_base2 = len(base2)
+
+for i in range(len(Esp1)):
+    aux = Esp2 + []
+    for k in range(len(base1[0])):
+        for j in range(len(Esp2)):
+            aux[j] = aux[j].replace(Esp1[k], str(base1[i][k]))
+    matrizAsociada.append(aux)
+
+for fila in range(len(matrizAsociada)):
+    for col in range(len(matrizAsociada[0])):
+        matrizAsociada[fila][col] = int(eval(matrizAsociada[fila][col]))
+
+x, y, z, k = symbols("x, y, z, k")
+if dim_base2 == 0:
+    parametros = []
+elif dim_base2 == 1:
+    parametros = [x]
+elif dim_base2 == 2:
+    parametros = [x, y]
+elif dim_base2 == 3:
+    parametros = [x, y, z]
+elif dim_base2 == 4:
+    parametros = [x, y, z, k]
+else:
+    sys.exit("No se adminte (sub)espacio de llegada W de dimensión mayor a 4")
+
+matrizSol = []
+A = Matrix(base2)
+print("\n")
+for fil in range(len(matrizAsociada)):
+    b = Matrix(matrizAsociada[fil])
+    solucion = iter(linsolve((A, b), parametros))
+    solucion = str(next(solucion))
+    solucion = leerSolucion(solucion)
+    matrizSol.append(solucion)
+
+matrizAsociada = transpuesta(matrizSol)
+print("La matriz asociada calculada es:\n")
+for fila in range(len(matrizAsociada)):
+    print(matrizAsociada[fila])
---
+++ venv/ModuloMatrices_Aplicaciones.py
@@ -0,0 +1,53 @@
+# -*- coding: iso-8859-15 -*-
+#
+# VERSION 1.0 (28-Enero-2022)
+#
+# AYUDA del módulo 'modulomatrizasociada'. Define 4 funciones,
+# que podemos utilizar de la siguiente forma:
+#
+# - espaciosVectoriales = leerAplicacionLineal(aplicacionLineal). Lee la aplicación lineal
+#   indicada y la devuelve los espacios vectoriales de partida y llegada en una lista
+#
+# - Base = leerBase(base). Lee la base indicada y devuelve esa misma base en una lista
+#
+# - transpuesta(matriz): Lee la matriz y calcula su transpuesta
+#
+# - leerSolucion(solucion): Lee la solucion con el formato (num1, num2, num3, ...)
+#   y crea su correspondiente lista
+
+def leerAplicacionLineal(aplicacion):
+    datosApl = aplicacion.split()
+    datosApl[0] = datosApl[0][1:-1]
+    datosApl[2] = datosApl[2][1:-1]
+    Esp1 = datosApl[0].split(",")
+    Esp2 = datosApl[2].split(",")
+    return [Esp1, Esp2]
+
+def leerBase(base):
+    lista = []
+    base = base[1:-1]
+    base = base.split()
+    for i in range(len(base)):
+        base[i] = base[i][1:-1]
+        vector = base[i].split(",")
+        for j in range(len(vector)):
+            vector[j] = int(vector[j])
+        lista.append(vector)
+    return lista
+
+def transpuesta(matriz):
+    t = []
+    for i in range(len(matriz[0])):
+        t.append([])
+        for j in range(len(matriz)):
+            t[i].append(matriz[j][i])
+    return t
+
+def leerSolucion(solucion):
+    solucion = solucion[:-1]
+    datos = solucion.split(",")
+    matsol = []
+    for i in range(len(datos)):
+        unaSol = datos[i][1:]
+        matsol.append(unaSol)
+    return matsol
